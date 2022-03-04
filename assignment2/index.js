687b14b5e74a9439f06759956bf2a2515fea5363
shindedarshan502 <96101838+shindedarshan502@users.noreply.github.com>
2022-03-04 15:01:33 +0530

    y

---
+++ assignment2/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+// console.log(express)
+
+const app = express()
+// console.log(app)
+
+app.get("/abc",function(req, res){
+    console.log("hello")
+
+    res.send("HELLO")
+})
+app.get("/books",function(req, res){
+    res.json({
+        "title": "Flowers",
+        "authors": [
+            "Vijaya Khisty Bodach"
+           ],
+           "title2": "The Girl who Played with Fire",
+        "authors2": [
+            "Stieg Larsson"
+           ],
+           
+        "title3": "The Sign of Four",
+        "authors3": [
+            "Sir Arthur Conan Doyle"
+           ],
+    },
+    )
+    // console.log(res)
+})
+
+app.listen(3000,() =>{
+    console.log("listening")
+})
