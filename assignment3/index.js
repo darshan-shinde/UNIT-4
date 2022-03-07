8b117a6d33a173754d2502125399d22e5bbcf8f4
shindedarshan502 <96101838+shindedarshan502@users.noreply.github.com>
2022-03-07 19:52:39 +0530

    y

---
+++ assignment3/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const app = express()
+
+app.get("/books",allbooks,(req, res) => {
+    return res.send("all books")
+})
+
+app.listen(3000,() => {
+    console.log("Listening")
+})
+
+function allbooks(req, res, next) {
+   
+    console.log("fetching all books")
+    next();
+
+}
+
+app.get("/books/:name",singlebooks,(req, res) => {
+    return res.send({bookName : req.name})
+})
+
+function singlebooks(req, res, next) {
+    console.log("books")
+    req.name = "GamesOfThrones"
+    // req.name = req.params.name
+    console.log(req.name)
+    next();
+}
