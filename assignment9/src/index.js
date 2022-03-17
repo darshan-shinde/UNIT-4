1ec9277ffba2e7355ef8af44616d59aa77f444c0
shindedarshan502 <96101838+shindedarshan502@users.noreply.github.com>
2022-03-17 10:11:36 +0530

    y

---
+++ assignment9/src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const usersController = require("../../assignment9/src/controllers/user.controllers");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/users", usersController);
+
+module.exports = app;
