a3c66abd6151324264f151128a9b6427017f1639
shindedarshan502 <96101838+shindedarshan502@users.noreply.github.com>
2022-03-23 12:36:21 +0530

    y

--- assignment11/src/configs/db.js
+++ assignment11/src/configs/db.js
@@ -1,9 +1,5 @@
-const express = require("express")
-
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 module.exports = () => {
-    return mongoose.connect(
-        "mongodb://127.0.0.1:27017/user"
-    );
+    return mongoose.connect("mongodb+srv://nihal1234:nihal1234@nihalcluster.p9poj.mongodb.net/web15?retryWrites=true&w=majority");
 };
